bd0f9084160ed28553a9a55141016fc9989f95c1
Mathieu Tarral <mathieu.tarral@f-secure.com>
2017-02-16 12:41:57 +0200

    fix memory-access.h merge conflict

--- memory-access.h
+++ memory-access.h
@@ -7,10 +7,7 @@
 #ifndef MEMORY_ACCESS_H
 #define MEMORY_ACCESS_H
 
-<<<<<<< ours
-=======
 #include <sys/socket.h>
->>>>>>> theirs
 #include "qapi-types.h"
 #include "qapi/qmp/qdict.h"
 #include "qapi/error.h"
@@ -18,16 +15,11 @@
 void qmp_pmemaccess (const char *path, Error **errp);
 
 struct pmemaccess_args {
-<<<<<<< ours
-	char *path;
-	Error **errp;
-=======
     int socket_fd;
     struct sockaddr_un *address;
     socklen_t address_length;
     char *path;
     Error **errp;
->>>>>>> theirs
 };
 
 #endif /* MEMORY_ACCESS_H */
