62e506c2c1489ee8b293cfe70bd9c368063752a9
Mathieu Tarral <mathieu.tarral@gmail.com>
2017-02-27 21:22:04 +0200

    change request type to uint64_t

--- memory-access.c
+++ memory-access.c
@@ -22,7 +22,7 @@
 #include "exec/cpu-common.h"
 
 struct request{
-    uint8_t type;      // 0 quit, 1 read, 2 write, ... rest reserved
+    uint64_t type;      // 0 quit, 1 read, 2 write, ... rest reserved
     uint64_t address;  // address to read from OR write to
     uint64_t length;   // number of bytes to read OR write
 };
